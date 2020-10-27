e971f23a3a47a902af2bfc37dfc4d0130dee5c6f
root <root@l-csi-0103s.mtl.labs.mlnx>
2020-10-27 21:57:56 +0200

    Added LIF and LPPFX in iperf_server2.sh and added comments to client and server scripts.

--- perf/iperf3_client2.sh
+++ perf/iperf3_client2.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 NoT=${1:-1}
-REMOTE_SERVER=l-csi-0104s
 RUNTIME=60
 FIFO_DIR=/root/ipsec/fifos/
 
+# BF2 NUMA cores.  Change the following to the BF2's NUMA cores.
 res=($(seq 12 23) $(seq 36 47) $(seq 0 11) $(seq 24 35))
 for i in $( seq 0 $(( NoT - 1)) )
 do
+# iperf3 server IP address.
     IPADDR=2.2.$i.2
     coren=${res[$i]}
     cmd="taskset -c $coren iperf3 -c $IPADDR -M 1350 --logfile ${FIFO_DIR}fifo${i} -t $RUNTIME &"
--- perf/iperf3_server2.sh
+++ perf/iperf3_server2.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
+LIF=enp175s0f0
+LPPFX=enp
 
 NoT=${1:-1}
-IPs=(enp175s0f0 `ibdev2netdev -v | sort -t : -k3,3 | awk '{print $12}' | grep enp`)
+IPs=($LIF `ibdev2netdev -v | sort -t : -k3,3 | awk '{print $12}' | grep $LPPFX`)
+# BF2 NUMA cores.  Change the following to the BF2's NUMA cores.
 res=($(seq 12 23) $(seq 36 47) $(seq 0 11) $(seq 24 35))
+
 for i in $( seq 0 $(( NoT - 1)) )
 do
     IPADDR=`ip addr show ${IPs[$i]} | grep global | awk '{print $2}'`
